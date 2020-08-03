c419a80e21fdd5f80267e12c8755cd282ae89290
dengke.li <2009lidengke@163.com>
2020-08-03 18:15:23 +0800

    Update githubusercontent.com.js

--- qx/js/githubusercontent.com.js
+++ qx/js/githubusercontent.com.js
@@ -1,3 +1,8 @@
+/**
+hostname = raw.githubusercontent.com, gist.githubusercontent.com
+# 处理响应头部
+^https://(?:raw|gist)\.githubusercontent.com/.+\.js url script-response-body https://raw.githubusercontent.com/nameldk/rules/master/qx/js/githubusercontent.com.js
+*/
 
 let modifiedBody = $response.body;
 let modifiedHeaders = $response.headers;
